3e00e3d1cfd71dd656ab0984198fac21bc952e23
ferundal <46624679+Ferundal@users.noreply.github.com>
2022-11-05 20:31:17 +0400

    Animation fixed, house added.

---
+++ Assets/Scripts/UI/PromptText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class PromptText : MonoBehaviour
+    {
+        [TextArea(15,20)]
+        [SerializeField] private string promptText;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player") || other.CompareTag("Fire"))
+            {
+                PromptManager promptManager = FindObjectOfType<PromptManager>();
+                promptManager.GetNewPrompt(promptText);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
